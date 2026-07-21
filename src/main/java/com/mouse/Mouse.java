e831d23f8c5c19b45a47842e6a584861eef858d5
spoon-mouse <spoon@mouse.com>
2026-07-21 14:36:38 +0100

    mouse

--- src/main/java/com/mouse/Mouse.java
+++ src/main/java/com/mouse/Mouse.java
@@ -1,22 +1,25 @@
 package com.mouse;
 
-import org.bitcoinj.base.Network;
 import org.bitcoinj.base.ScriptType;
 import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.params.BitcoinNetworkParams;
 import org.bitcoinj.params.TestNet3Params;
-import org.bitcoinj.protobuf.wallet.Protos;
-import org.bitcoinj.script.Script;
 import org.bitcoinj.wallet.Wallet;
 
 public class Mouse {
 
+    record Point(int x, int y) {}
+
     public static void main(String[] args){
+
+        Point p = new Point(1,1);
+
+
         System.out.println("mouse");
 
         NetworkParameters params = TestNet3Params.get();
         Wallet wallet = Wallet.createDeterministic(params, ScriptType.P2PKH);
         //System.out.println(wallet.getKeyChainSeed().getMnemonicString());
+
 
     }
 }
