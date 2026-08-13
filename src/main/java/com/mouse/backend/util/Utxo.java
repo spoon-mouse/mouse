ec1f161d032ebb1eaa5b5dd378efc472fc7a02ea
spoon-mouse <spoon@mouse.com>
2026-08-13 01:35:52 +0100

    just kit

--- src/main/java/com/mouse/backend/util/Utxo.java
+++ src/main/java/com/mouse/backend/util/Utxo.java
@@ -2,6 +2,6 @@ package com.mouse.backend.util;
 
 import org.bitcoinj.base.Sha256Hash;
 
-public record Utxo(String txId, int outputIdx, String address, boolean scv, long relLock, long value, long depth){
+public record Utxo(String txId, int idx, String address, boolean scv, long relLock, long value, long depth){
 
 }
