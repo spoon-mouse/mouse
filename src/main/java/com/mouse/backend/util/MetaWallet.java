983f817f63f15e382cb1a54e3f55a1ac6c3a59e1
spoon-mouse <spoon@mouse.com>
2026-09-04 07:51:52 +0100

    add encryption and decryption methods, and retrieve mnemonic and seed creation time

--- src/main/java/com/mouse/backend/util/MetaWallet.java
+++ src/main/java/com/mouse/backend/util/MetaWallet.java
@@ -3,6 +3,9 @@ package com.mouse.backend.util;
 import org.bitcoinj.base.Sha256Hash;
 import org.bitcoinj.wallet.Wallet;
 
+import java.nio.CharBuffer;
+import java.util.List;
+
 
 public record MetaWallet(Wallet wallet, String name, String id) {
 
@@ -32,6 +35,24 @@ public record MetaWallet(Wallet wallet, String name, String id) {
 
     public String receiveAddress(){
         return wallet.currentReceiveAddress().toString();
+    }
+
+    public void encrypt(char[] password) {
+        wallet.encrypt(CharBuffer.wrap(password));
+    }
+
+    public void decrypt(char[] password) {
+        wallet.decrypt(CharBuffer.wrap(password));
+    }
+
+    public List<String> getMnemonic() {
+        if (wallet.getKeyChainSeed() == null) return null;
+        return wallet.getKeyChainSeed().getMnemonicCode();
+    }
+
+    public long getSeedCreationTime() {
+        if (wallet.getKeyChainSeed() == null) return 0L;
+        return wallet.getKeyChainSeed().getCreationTimeSeconds();
     }
 
 }
